c32ead33dc63dc657d16e1e35ce70da92332b4f9
ngknnnn <namkhanh2903@gmail.com>
2020-02-05 17:10:02 +0700

    complete Loadingpage

--- loading_page.js
+++ loading_page.js
@@ -1,4 +1,18 @@
-$(window).on('load',function(event){
-	$('body').removeClass('container');
-	$('loader').daley(2000).fadeOut('fast');
-});
+(function () {
+	var preload = document.getElementById("preload");
+	var loading = 0;
+	var id = setInterval(frame, 64);
+	function frame(){
+		if(loading==100){
+			clearInterval(id);
+			window.open("asd.html","_self");
+		}
+		else{
+			loading = loading +1;
+			if(loading == 90 ){
+				preload.style.animation = "fadeout 1s ease";
+			}
+		}
+	}
+
+})();
